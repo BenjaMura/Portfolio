20804e48c7381265390c72b2b57dcfecf405ab86
BenjaMura <benjaminmuratore1@gmail.com>
2024-01-21 21:30:08 -0300

    Stage 18 - fix render error sectionname

--- components/intro.tsx
+++ components/intro.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import React from "react";
-import waveImage from '@/public/Wave.gif';
+import waveImage from "@/public/Wave.gif";
 import { motion } from "framer-motion";
 import Link from "next/link";
 import { BsLinkedin } from "react-icons/bs";
@@ -11,16 +11,17 @@ import { FaGithubSquare } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
 import { useActiveSectionContext } from "@/context/active-section-context";
 import { useTranslation } from "react-i18next";
+import { SectionName } from "@/lib/types";
 
 export default function Intro() {
   const { t } = useTranslation();
-  const { ref } = useSectionInView(t('header1'), 0.5);
+  const { ref } = useSectionInView(t("header1"), 0.5);
   const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
 
   return (
     <section
       ref={ref}
-      id={t('header1.1')}
+      id={t("header1.1")}
       className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
     >
       <div className="flex items-center justify-center">
@@ -50,7 +51,11 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="flex items-center justify-center"><Image src={waveImage} alt="Wave" width={40} height={40} /></span><span className="font-bold">{t('intro1')}</span>{t('intro2')}
+        <span className="flex items-center justify-center">
+          <Image src={waveImage} alt="Wave" width={40} height={40} />
+        </span>
+        <span className="font-bold">{t("intro1")}</span>
+        {t("intro2")}
       </motion.h1>
 
       <motion.div
@@ -62,31 +67,36 @@ export default function Intro() {
         }}
       >
         <Link
-          href={t('header5.2')}
+          href={t("header5.2")}
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:bg-amber-50 hover:scale-110 active:scale-105 transition borderBlack dark:bg-zinc-600 dark:text-amber-200"
           onClick={() => {
-            setActiveSection(t('header5'));
+            const sectionName = t("header5") as SectionName;
+            setActiveSection(sectionName);
             setTimeOfLastClick(Date.now());
           }}
         >
-          {t('intro3')}
+          {t("intro3")}
         </Link>
 
-        {t('intro4') === "Descarga mi CV" ? <a
-          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:bg-amber-50 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-zinc-600 dark:text-amber-200"
-          href="/CV-Muratore-Benjamín-LAT.pdf"
-          download
-        >
-          {t('intro4')}{" "}
-          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
-        </a> : <a
-          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:bg-amber-50 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-zinc-600 dark:text-amber-200"
-          href="/CV-Muratore-Benjamin-ENG.pdf"
-          download
-        >
-          {t('intro4')}{" "}
-          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
-        </a> }
+        {t("intro4") === "Descarga mi CV" ? (
+          <a
+            className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:bg-amber-50 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-zinc-600 dark:text-amber-200"
+            href="/CV-Muratore-Benjamín-LAT.pdf"
+            download
+          >
+            {t("intro4")}{" "}
+            <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+          </a>
+        ) : (
+          <a
+            className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:bg-amber-50 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-zinc-600 dark:text-amber-200"
+            href="/CV-Muratore-Benjamin-ENG.pdf"
+            download
+          >
+            {t("intro4")}{" "}
+            <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+          </a>
+        )}
 
         <a
           className="bg-white p-4 text-zinc-700 hover:text-amber-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:bg-amber-50 hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-zinc-600 dark:text-amber-200"
