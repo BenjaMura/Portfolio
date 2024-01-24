3086d853b22b95e00039b550fed863e1fad894cb
BenjaMura <benjaminmuratore1@gmail.com>
2024-01-24 20:46:38 -0300

    Stage 38 - detecting bug step 3 config next

--- i18n/i18n.ts
+++ i18n/i18n.ts
@@ -1,10 +1,12 @@
 import { initReactI18next } from "react-i18next";
 import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 import es from "./locales/es/es.json";
 import en from "./locales/en/en.json";
 
 i18n
   .use(initReactI18next)
+  .use(LanguageDetector)
   .init({
     resources: {
       en: {
@@ -16,6 +18,13 @@ i18n
     },
     lng: "es",
     fallbackLng: "en",
+    detection: {
+      order: ["localStorage", "navigator"],
+      lookupLocalStorage: "lang",
+    },
+    interpolation: {
+      escapeValue: false,
+    },
   });
 
 export default i18n;
